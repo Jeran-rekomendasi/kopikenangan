ecf46b6f28380f23faa40e8ed40d517b681f101a
Jeran-rekomendasi <jeranmaneoto@gmail.com>
2024-10-31 08:59:38 +0700

    Add files via upload

---
+++ latihan website/js/script.js
@@ -0,0 +1,16 @@
+// toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+// ketika menu diklik
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar utk menghilangkan nav
+const hamburger = document.querySelector('#menu');
+
+document.addEventListener('click', function (e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
